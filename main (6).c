2bf4eddd1f1f3127a0e031005ecf810c7d5937cf
Harshamnnayaka07 <harshamnnayaka07@gmail.com>
2024-11-26 21:44:41 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    // do-while loop starts here
+    do {
+        printf("Iteration: %d\n", i); // Print the current value of i
+        i++; // Increment i by 1
+    } while (i <= 5); // Condition for the loop to continue
+
+    return 0;
+}
